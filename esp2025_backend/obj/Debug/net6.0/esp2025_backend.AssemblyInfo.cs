0ca0d798c45bd0cfdb460ec753128c082a0c7c9c
JeremieLapointe13 <2141363@cegeprdl.ca>
2025-02-10 08:55:03 -0500

    backend changes

--- esp2025_backend/obj/Debug/net6.0/esp2025_backend.AssemblyInfo.cs
+++ esp2025_backend/obj/Debug/net6.0/esp2025_backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("esp2025_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abef075e9e1525bd135145bc1370f321df42b9c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30e0e844b3f5c744a250332388ebc30bf9db742")]
 [assembly: System.Reflection.AssemblyProductAttribute("esp2025_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("esp2025_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
