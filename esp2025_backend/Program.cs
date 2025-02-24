acd00085587b83385717304aa06853e1b32e0de6
JeremieLapointe13 <2141363@cegeprdl.ca>
2025-02-24 08:43:51 -0500

    Changements pour la connexion à la DB sur le VPS

--- esp2025_backend/Program.cs
+++ esp2025_backend/Program.cs
@@ -26,7 +26,7 @@ namespace esp2025_backend
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
             {
                 app.UseSwagger();
                 app.UseSwaggerUI();
